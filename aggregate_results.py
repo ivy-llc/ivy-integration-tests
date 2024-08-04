4e202a17796ed7b4fc9b88657931350b29b7291d
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-08-04 07:08:48 +0100

    debug

--- aggregate_results.py
+++ aggregate_results.py
@@ -7,22 +7,22 @@ output_file = 'aggregated-results/all-test-results.txt'
 # Ensure the output directory exists
 os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
-with open(output_file, 'w') as outfile:
-    for subdir, _, files in os.walk(artifacts_dir):
-        for file in files:
-            print('file', file)
-            # if file.startswith('test-results-'):
-            # print('file', file)
-            file_path = os.path.join(subdir, file)
-            print('subdir', subdir)
-            print('file_path', file_path)
-            infile = open(file_path, "r")
-            print('infile:', infile)
-            print(infile.read())
-            with open(file_path, 'r') as infile:
-                print('file contents', infile.read())
-                outfile.write(infile.read())
-                outfile.write('\n')
+outfile = open(output_file, 'a')
+
+for subdir, _, files in os.walk(artifacts_dir):
+    for file in files:
+        print('file', file)
+        # if file.startswith('test-results-'):
+        # print('file', file)
+        file_path = os.path.join(subdir, file)
+        print('subdir', subdir)
+        print('file_path', file_path)
+        infile = open(file_path, "r")
+        print('file contents', infile.read())
+        for line in infile:
+            print('writing line', line)
+            outfile.write(line)
+outfile.close()
 
 with open(output_file, 'r') as outfile:
     print('lines of out file:')
