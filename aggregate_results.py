841a5ba956aecf4429a387d0df85a295cfdeab18
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-08-04 02:56:24 +0100

    debug

--- aggregate_results.py
+++ aggregate_results.py
@@ -10,6 +10,7 @@ os.makedirs(os.path.dirname(output_file), exist_ok=True)
 with open(output_file, 'w') as outfile:
     for subdir, _, files in os.walk(artifacts_dir):
         for file in files:
+            print('file', file)
             if file.startswith('test-results-'):
                 file_path = os.path.join(subdir, file)
                 with open(file_path, 'r') as infile:
