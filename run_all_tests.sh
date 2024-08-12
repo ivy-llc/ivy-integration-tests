65ef8a0e5c5cb1ed72c7d2a9275e42f7120d5120
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-08-12 06:48:45 +0100

    add workflow for running all tests not in parallel

---
+++ run_all_tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+binaries=$1
+
+export VERSION=$binaries  # set the branch to pull the binaries from
+
+pip3 install -e ivy/
+cd ivy-integration-tests
+pip3 install -r requirements.txt
+
+if [ "$integration" = "transformers" ]; then
+    pip3 install tensorflow==2.15.1
+    pip3 install datasets
+    pip3 install transformers
+fi
+
+# get the nightly binaries
+python << 'EOF'
+import ivy
+ivy.utils.cleanup_and_fetch_binaries()
+EOF
+
+# runs the tests on the latest ivy commit, and the linux binaries that are built nightly
+set +e
+pytest --source-to-source -p no:warnings --tb=no
+pytest_exit_code=$?
+set -e
+
+if [ $pytest_exit_code -eq 0 ] || [ $pytest_exit_code -eq 1 ]; then
+    exit 0
+else
+    exit 1
+fi
