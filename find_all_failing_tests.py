0b83c3ae340a3ac8a50e2b162247bd0e5b178cbc
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-08-12 08:20:48 +0100

    add test logging to s2s dashboard update workflow

--- find_all_failing_tests.py
+++ find_all_failing_tests.py
@@ -3,39 +3,47 @@ import os
 
 if __name__ == "__main__":
     failing_tests = []
+    test_logs = ""
 
     for subdir, _, files in os.walk("artifacts"):
         for file_name in files:
             file_path = os.path.join(subdir, file_name)
 
             with open(file_path, "r") as file:
-                for line in file.readlines():
-                    split_line = line.split(",")[:-1]
-                    if len(split_line) != 6:
-                        continue
-                    record = {
-                        "target": split_line[0],
-                        "mode": split_line[1],
-                        "backend_compile": split_line[2],
-                        "function": split_line[3],
-                        "workflow_link": split_line[4],
-                        "outcome": split_line[5],
-                    }
+                if "_results" in file_name:
+                    for line in file.readlines():
+                        split_line = line.split(",")[:-1]
+                        if len(split_line) != 6:
+                            continue
+                        record = {
+                            "target": split_line[0],
+                            "mode": split_line[1],
+                            "backend_compile": split_line[2],
+                            "function": split_line[3],
+                            "workflow_link": split_line[4],
+                            "outcome": split_line[5],
+                        }
 
-                    target = record["target"]
-                    mode = record["mode"]
-                    backend_compile = record["backend_compile"]
-                    function = record["function"]
-                    outcome = record['outcome']
-                    workflow_link = record['workflow_link']
+                        target = record["target"]
+                        mode = record["mode"]
+                        backend_compile = record["backend_compile"]
+                        function = record["function"]
+                        outcome = record['outcome']
+                        workflow_link = record['workflow_link']
 
-                    backend_compile = "T" in backend_compile.upper()  # convert to bool
+                        backend_compile = "T" in backend_compile.upper()  # convert to bool
 
-                    if outcome == "failed":
-                        failing_tests.append(f"Obj: {function} | Target: {target} | Native Compiled: {backend_compile}\n")
+                        if outcome == "failed":
+                            failing_tests.append(f"Obj: {function} | Target: {target} | Native Compiled: {backend_compile}\n")
+
+                elif "_logs" in file_name:
+                    test_logs += file.read() + "\n"
 
     failing_tests = sorted(failing_tests)
 
     with open("FAILING_TESTS.txt", "w") as out_file:
         for line in failing_tests:
             out_file.write(line)
+    
+    with open("TEST_LOGS.txt", "w") as out_file:
+        out_file.write(test_logs)
--- update_s2s_dashboard.py
+++ update_s2s_dashboard.py
@@ -26,59 +26,60 @@ if __name__ == "__main__":
 
     for subdir, _, files in os.walk("artifacts"):
         for file_name in files:
-            file_path = os.path.join(subdir, file_name)
+            if "_results" in file_name:
+                file_path = os.path.join(subdir, file_name)
 
-            with open(file_path, "r") as file:
-                for line in file.readlines():
-                    split_line = line.split(",")[:-1]
-                    if len(split_line) != 6:
-                        continue
-                    record = {
-                        "target": split_line[0],
-                        "mode": split_line[1],
-                        "backend_compile": split_line[2],
-                        "function": split_line[3],
-                        "workflow_link": split_line[4],
-                        "outcome": split_line[5],
-                    }
-
-                    target = record["target"]
-                    mode = record["mode"]
-                    backend_compile = record["backend_compile"]
-                    function = record["function"]
-                    outcome = record['outcome']
-                    workflow_link = record['workflow_link']
-
-                    backend_compile = "T" in backend_compile.upper()  # convert to bool
-                    if backend_compile != args.backend_compile:
-                        # do not consider this test, as it uses the wrong setting of backend_compile
-                        continue
-
-                    if outcome == "passed":
-                        passed += 1
-                    else:
-                        failed += 1
-
-                    if function not in test_outcomes:
-                        test_outcomes[function] = {
-                            "jax": False,
-                            "numpy": False,
-                            "tensorflow": False,
+                with open(file_path, "r") as file:
+                    for line in file.readlines():
+                        split_line = line.split(",")[:-1]
+                        if len(split_line) != 6:
+                            continue
+                        record = {
+                            "target": split_line[0],
+                            "mode": split_line[1],
+                            "backend_compile": split_line[2],
+                            "function": split_line[3],
+                            "workflow_link": split_line[4],
+                            "outcome": split_line[5],
                         }
 
-                    test_outcomes[function][target if mode in ["transpile", "s2s"] else "trace"] = outcome == "passed"
+                        target = record["target"]
+                        mode = record["mode"]
+                        backend_compile = record["backend_compile"]
+                        function = record["function"]
+                        outcome = record['outcome']
+                        workflow_link = record['workflow_link']
 
-                    split_fn = function.split(".")
-                    integration = split_fn[0]
-                    submodule = split_fn[1] if len(split_fn) > 2 else ""
-                    if integration in ["transformers"]:
-                        # remove the subsection name here, as it is not actually the submodule in these cases
-                        function = integration + "." + split_fn[-1]
+                        backend_compile = "T" in backend_compile.upper()  # convert to bool
+                        if backend_compile != args.backend_compile:
+                            # do not consider this test, as it uses the wrong setting of backend_compile
+                            continue
 
-                    color = color_codes.get(outcome, 'yellow')
-                    button = f"[![{outcome}](https://img.shields.io/badge/{outcome}-{color})]({workflow_link})"
-                    if workflow_link not in [None, "null"]:
-                        test_results[integration][submodule][function][target if mode in ["transpile", "s2s"] else "trace_graph"] = button
+                        if outcome == "passed":
+                            passed += 1
+                        else:
+                            failed += 1
+
+                        if function not in test_outcomes:
+                            test_outcomes[function] = {
+                                "jax": False,
+                                "numpy": False,
+                                "tensorflow": False,
+                            }
+
+                        test_outcomes[function][target if mode in ["transpile", "s2s"] else "trace"] = outcome == "passed"
+
+                        split_fn = function.split(".")
+                        integration = split_fn[0]
+                        submodule = split_fn[1] if len(split_fn) > 2 else ""
+                        if integration in ["transformers"]:
+                            # remove the subsection name here, as it is not actually the submodule in these cases
+                            function = integration + "." + split_fn[-1]
+
+                        color = color_codes.get(outcome, 'yellow')
+                        button = f"[![{outcome}](https://img.shields.io/badge/{outcome}-{color})]({workflow_link})"
+                        if workflow_link not in [None, "null"]:
+                            test_results[integration][submodule][function][target if mode in ["transpile", "s2s"] else "trace_graph"] = button
 
     fns_passing_all_targets = 0
     fns_passing_jax = 0
