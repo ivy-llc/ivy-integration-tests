621755a089639ce4f381c04c353745c9c141f512
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-08-16 00:02:24 +0100

    add some kornia.contrib class tests

--- kornia/test_contrib.py
+++ kornia/test_contrib.py
@@ -1,5 +1,15 @@
-from helpers import _test_function
+from helpers import (
+    _array_to_new_backend,
+    _check_allclose,
+    _check_shape_allclose,
+    _nest_array_to_numpy,
+    _nest_torch_tensor_to_new_framework,
+    _test_function,
+)
+
+import ivy
 import kornia
+import pytest
 import torch
 
 
@@ -137,3 +147,105 @@ def test_diamond_square(target_framework, mode, backend_compile):
         tolerance=1e-3,
         mode=mode,
     )
+
+
+def test_EdgeDetector(target_framework, mode, backend_compile):
+    print("kornia.contrib.EdgeDetector")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledEdgeDetector = ivy.transpile(kornia.contrib.EdgeDetector, source="torch", target="tensorflow")
+
+    torch_detector = kornia.contrib.EdgeDetector()
+    transpiled_detector = TranspiledEdgeDetector()
+
+    torch_args = (
+        torch.rand(1, 3, 320, 320),
+    )
+    torch_out = torch_detector(*torch_args)
+
+    transpiled_args = _nest_torch_tensor_to_new_framework(torch_args, target_framework)
+    transpiled_out = transpiled_detector(*transpiled_args)
+
+    orig_np = _nest_array_to_numpy(torch_out)
+    transpiled_np = _nest_array_to_numpy(transpiled_out)
+    _check_allclose(orig_np, transpiled_np)
+
+
+def test_FaceDetector(target_framework, mode, backend_compile):
+    print("kornia.contrib.FaceDetector")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledFaceDetector = ivy.transpile(kornia.contrib.FaceDetector, source="torch", target="tensorflow")
+
+    torch_detector = kornia.contrib.FaceDetector()
+    transpiled_detector = TranspiledFaceDetector()
+
+    torch_args = (
+        torch.rand(1, 3, 320, 320),
+    )
+    torch_out = torch_detector(*torch_args)
+
+    transpiled_args = _nest_torch_tensor_to_new_framework(torch_args, target_framework)
+    transpiled_out = transpiled_detector(*transpiled_args)
+
+    orig_np = _nest_array_to_numpy(torch_out)
+    transpiled_np = _nest_array_to_numpy(transpiled_out)
+    _check_allclose(orig_np, transpiled_np)
+
+
+def test_VisionTransformer(target_framework, mode, backend_compile):
+    print("kornia.contrib.VisionTransformer")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledVisionTransformer = ivy.transpile(kornia.contrib.VisionTransformer, source="torch", target="tensorflow")
+
+    torch_vit = kornia.contrib.VisionTransformer()
+    transpiled_vit = TranspiledVisionTransformer()
+
+    torch_args = (
+        torch.rand(1, 3, 320, 320),
+    )
+    torch_out = torch_vit(*torch_args)
+
+    transpiled_args = _nest_torch_tensor_to_new_framework(torch_args, target_framework)
+    transpiled_out = transpiled_vit(*transpiled_args)
+
+    orig_np = _nest_array_to_numpy(torch_out)
+    transpiled_np = _nest_array_to_numpy(transpiled_out)
+    _check_shape_allclose(orig_np, transpiled_np)
+
+
+def test_KMeans(target_framework, mode, backend_compile):
+    print("kornia.contrib.KMeans")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledKMeans = ivy.transpile(kornia.contrib.KMeans, source="torch", target="tensorflow")
+
+    torch_kmeans = kornia.contrib.KMeans(3, None, 10e-4, 100, 0)
+    transpiled_kmeans = TranspiledKMeans(3, None, 10e-4, 100, 0)
+    
+    torch_x1 = torch.rand((1000, 5))
+    torch_x2 = torch.rand((10, 5))
+    transpiled_x1 = _array_to_new_backend(torch_x1, target_framework)
+    transpiled_x2 = _array_to_new_backend(torch_x2, target_framework)
+
+    torch_kmeans.fit(torch_x1)
+    torch_predictions = torch_kmeans.predict(torch_x2)
+
+    transpiled_kmeans.fit(transpiled_x1)
+    transpiled_predictions = transpiled_kmeans.predict(transpiled_x2)
+
+    orig_np = _nest_array_to_numpy(torch_predictions)
+    transpiled_np = _nest_array_to_numpy(transpiled_predictions)
+    _check_shape_allclose(orig_np, transpiled_np)
+
+
+# TODO: there are more classes in kornia.contrib that are not being tested yet
