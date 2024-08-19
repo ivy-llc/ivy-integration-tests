91e42ea43f3ba24199733b4a80d21fa104cedbe3
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-08-19 10:41:23 +0100

    add missing kornia.filters tests

--- kornia/test_filters.py
+++ kornia/test_filters.py
@@ -1,5 +1,13 @@
-from helpers import _test_function
+from helpers import (
+    _nest_torch_tensor_to_new_framework,
+    _to_numpy_and_allclose,
+    _to_numpy_and_shape_allclose,
+    _test_function,
+)
+
+import ivy
 import kornia
+import pytest
 import torch
 
 
@@ -654,3 +662,280 @@ def test_get_motion_kernel2d(target_framework, mode, backend_compile):
         tolerance=1e-3,
         mode=mode,
     )
+
+
+def test_Laplacian(target_framework, mode, backend_compile):
+    print("kornia.filters.Laplacian")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledLaplacian = ivy.transpile(kornia.filters.Laplacian, source="torch", target=target_framework)
+
+    x = torch.rand(2, 4, 5, 5)
+    torch_out = kornia.filters.Laplacian(kernel_size=3)(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledLaplacian(kernel_size=3)(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_Sobel(target_framework, mode, backend_compile):
+    print("kornia.filters.Sobel")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledSobel = ivy.transpile(kornia.filters.Sobel, source="torch", target=target_framework)
+
+    x = torch.rand(1, 3, 4, 4)
+    torch_out = kornia.filters.Sobel()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledSobel()(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_Canny(target_framework, mode, backend_compile):
+    print("kornia.filters.Canny")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledCanny = ivy.transpile(kornia.filters.Canny, source="torch", target=target_framework)
+
+    x = torch.rand(5, 3, 4, 4)
+    torch_out_magnitude, torch_out_edges = kornia.filters.Canny()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out_magnitude, transpiled_out_edges = TranspiledCanny()(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out_magnitude, transpiled_out_magnitude)
+    _to_numpy_and_allclose(torch_out_edges, transpiled_out_edges)
+
+
+def test_SpatialGradient(target_framework, mode, backend_compile):
+    print("kornia.filters.SpatialGradient")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledSpatialGradient = ivy.transpile(kornia.filters.SpatialGradient, source="torch", target=target_framework)
+
+    x = torch.rand(1, 3, 4, 4)
+    torch_out = kornia.filters.SpatialGradient()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledSpatialGradient()(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_SpatialGradient3d(target_framework, mode, backend_compile):
+    print("kornia.filters.SpatialGradient3d")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledSpatialGradient3d = ivy.transpile(kornia.filters.SpatialGradient3d, source="torch", target=target_framework)
+
+    x = torch.rand(1, 4, 2, 4, 4)
+    torch_out = kornia.filters.SpatialGradient3d()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledSpatialGradient3d()(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_DexiNed(target_framework, mode, backend_compile):
+    print("kornia.filters.DexiNed")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledDexiNed = ivy.transpile(kornia.filters.DexiNed, source="torch", target=target_framework)
+
+    x = torch.rand(1, 3, 320, 320)
+    torch_out = kornia.filters.DexiNed(pretrained=False)(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledDexiNed(pretrained=False)(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out[-1], transpiled_out[-1])
+
+
+def test_BilateralBlur(target_framework, mode, backend_compile):
+    print("kornia.filters.BilateralBlur")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledBilateralBlur = ivy.transpile(kornia.filters.BilateralBlur, source="torch", target=target_framework)
+
+    x = torch.rand(2, 3, 5, 5)
+    torch_out = kornia.filters.BilateralBlur((3, 3), 0.1, (1.5, 1.5))(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledBilateralBlur((3, 3), 0.1, (1.5, 1.5))(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_BlurPool2D(target_framework, mode, backend_compile):
+    print("kornia.filters.BlurPool2D")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledBlurPool2D = ivy.transpile(kornia.filters.BlurPool2D, source="torch", target=target_framework)
+
+    x = torch.eye(5)[None, None]
+    torch_out = kornia.filters.BlurPool2D(kernel_size=3, stride=2)(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledBlurPool2D(kernel_size=3, stride=2)(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_BoxBlur(target_framework, mode, backend_compile):
+    print("kornia.filters.BoxBlur")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledBoxBlur = ivy.transpile(kornia.filters.BoxBlur, source="torch", target=target_framework)
+
+    x = torch.rand(2, 3, 5, 5)
+    torch_out = kornia.filters.BoxBlur(kernel_size=(3, 3))(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledBoxBlur(kernel_size=(3, 3))(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_MaxBlurPool2D(target_framework, mode, backend_compile):
+    print("kornia.filters.MaxBlurPool2D")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledMaxBlurPool2D = ivy.transpile(kornia.filters.MaxBlurPool2D, source="torch", target=target_framework)
+
+    x = torch.eye(5)[None, None]
+    torch_out = kornia.filters.MaxBlurPool2D(kernel_size=3, stride=2, max_pool_size=2, ceil_mode=False)(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledMaxBlurPool2D(kernel_size=3, stride=2, max_pool_size=2, ceil_mode=False)(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_MedianBlur(target_framework, mode, backend_compile):
+    print("kornia.filters.MedianBlur")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledMedianBlur = ivy.transpile(kornia.filters.MedianBlur, source="torch", target=target_framework)
+
+    x = torch.rand(2, 3, 5, 5)
+    torch_out = kornia.filters.MedianBlur(kernel_size=(3, 3))(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledMedianBlur(kernel_size=(3, 3))(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_GaussianBlur2d(target_framework, mode, backend_compile):
+    print("kornia.filters.GaussianBlur2d")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledGaussianBlur2d = ivy.transpile(kornia.filters.GaussianBlur2d, source="torch", target=target_framework)
+
+    x = torch.rand(2, 3, 5, 5)
+    torch_out = kornia.filters.GaussianBlur2d((3, 3), (1.5, 1.5))(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledGaussianBlur2d((3, 3), (1.5, 1.5))(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_GuidedBlur(target_framework, mode, backend_compile):
+    print("kornia.filters.GuidedBlur")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledGuidedBlur = ivy.transpile(kornia.filters.GuidedBlur, source="torch", target=target_framework)
+
+    guidance = torch.rand(2, 3, 5, 5)
+    x = torch.rand(2, 3, 5, 5)
+    torch_out = kornia.filters.GuidedBlur(3, 0.1)(guidance, x)
+
+    transpiled_guidance = _nest_torch_tensor_to_new_framework(guidance, target_framework)
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledGuidedBlur(3, 0.1)(transpiled_guidance, transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_JointBilateralBlur(target_framework, mode, backend_compile):
+    print("kornia.filters.JointBilateralBlur")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledJointBilateralBlur = ivy.transpile(kornia.filters.JointBilateralBlur, source="torch", target=target_framework)
+
+    guidance = torch.rand(2, 3, 5, 5)
+    x = torch.rand(2, 3, 5, 5)
+    torch_out = kornia.filters.JointBilateralBlur((3, 3), 0.1, (1.5, 1.5))(guidance, x)
+
+    transpiled_guidance = _nest_torch_tensor_to_new_framework(guidance, target_framework)
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledJointBilateralBlur((3, 3), 0.1, (1.5, 1.5))(transpiled_guidance, transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_MotionBlur(target_framework, mode, backend_compile):
+    print("kornia.filters.MotionBlur")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledMotionBlur = ivy.transpile(kornia.filters.MotionBlur, source="torch", target=target_framework)
+
+    x = torch.rand(2, 3, 5, 7)
+    torch_out = kornia.filters.MotionBlur(3, 35., 0.5)(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledMotionBlur(3, 35., 0.5)(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_UnsharpMask(target_framework, mode, backend_compile):
+    print("kornia.filters.UnsharpMask")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledUnsharpMask = ivy.transpile(kornia.filters.UnsharpMask, source="torch", target=target_framework)
+
+    x = torch.rand(2, 3, 5, 5)
+    torch_out = kornia.filters.UnsharpMask((3, 3), (1.5, 1.5))(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledUnsharpMask((3, 3), (1.5, 1.5))(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
