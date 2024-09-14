f619399705092355c6bed9a264b1f6eb39c2da07
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-09-14 05:14:18 +0100

    parallelise kornia.feature tests so all the integrations tests run way faster

---
+++ kornia/test_feature1.py
@@ -0,0 +1,465 @@
+from helpers import (
+    _nest_torch_tensor_to_new_framework,
+    _test_function,
+    _to_numpy_and_allclose,
+    _to_numpy_and_shape_allclose,
+)
+
+import ivy
+import kornia
+import pytest
+import torch
+
+
+# Tests #
+# ----- #
+
+def test_gftt_response(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 1, 7, 7),
+    )
+    trace_kwargs = {'grads_mode': 'sobel', 'sigmas': None}
+    test_args = (
+        torch.rand(5, 1, 7, 7),
+    )
+    test_kwargs = {'grads_mode': 'sobel', 'sigmas': None}
+    _test_function(
+        kornia.feature.gftt_response,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_harris_response(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 1, 7, 7),
+    )
+    trace_kwargs = {'k': 0.04, 'grads_mode': 'sobel', 'sigmas': None}
+    test_args = (
+        torch.rand(5, 1, 7, 7),
+    )
+    test_kwargs = {'k': 0.04, 'grads_mode': 'sobel', 'sigmas': None}
+    _test_function(
+        kornia.feature.harris_response,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_hessian_response(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 1, 7, 7),
+    )
+    trace_kwargs = {'grads_mode': 'sobel', 'sigmas': None}
+    test_args = (
+        torch.rand(5, 1, 7, 7),
+    )
+    test_kwargs = {'grads_mode': 'sobel', 'sigmas': None}
+    _test_function(
+        kornia.feature.hessian_response,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_dog_response(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 1, 5, 5, 5),
+    )
+    trace_kwargs = {}
+    test_args = (
+        torch.rand(5, 1, 5, 5, 5),
+    )
+    test_kwargs = {}
+    _test_function(
+        kornia.feature.dog_response,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_dog_response_single(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 1, 5, 5),
+    )
+    trace_kwargs = {'sigma1': 1.0, 'sigma2': 1.6}
+    test_args = (
+        torch.rand(5, 1, 5, 5),
+    )
+    test_kwargs = {'sigma1': 0.5, 'sigma2': 1.2}
+    _test_function(
+        kornia.feature.dog_response_single,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_get_laf_descriptors(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 1, 32, 32),
+        torch.rand(1, 3, 2, 2),
+        kornia.feature.HardNet8(pretrained=False),
+    )
+    trace_kwargs = {'patch_size': 32, 'grayscale_descriptor': True}
+    test_args = (
+        torch.rand(5, 1, 32, 32),
+        torch.rand(5, 3, 2, 2),
+        kornia.feature.HardNet8(pretrained=False),
+    )
+    test_kwargs = {'patch_size': 32, 'grayscale_descriptor': True}
+    _test_function(
+        kornia.feature.get_laf_descriptors,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+# DCF: our torch.cdist implementation uses a for-loop. https://github.com/ivy-llc/ivy/blob/65548817e99a396461c1eec5cf4eb9453c125cde/ivy/functional/frontends/torch/miscellaneous_ops.py#L100
+def test_match_nn(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 128),
+        torch.rand(1, 128),
+    )
+    trace_kwargs = {}
+    test_args = (
+        torch.rand(5, 128),
+        torch.rand(5, 128),
+    )
+    test_kwargs = {}
+    _test_function(
+        kornia.feature.match_nn,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+        skip=True,
+    )
+
+
+def test_match_mnn(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 128),
+        torch.rand(1, 128),
+    )
+    trace_kwargs = {}
+    test_args = (
+        torch.rand(5, 128),
+        torch.rand(5, 128),
+    )
+    test_kwargs = {}
+    _test_function(
+        kornia.feature.match_mnn,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_match_snn(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 128),
+        torch.rand(1, 128),
+    )
+    trace_kwargs = {'th': 0.8}
+    test_args = (
+        torch.rand(5, 128),
+        torch.rand(5, 128),
+    )
+    test_kwargs = {'th': 0.8}
+    _test_function(
+        kornia.feature.match_snn,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_match_smnn(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 128),
+        torch.rand(1, 128),
+    )
+    trace_kwargs = {'th': 0.95}
+    test_args = (
+        torch.rand(5, 128),
+        torch.rand(5, 128),
+    )
+    test_kwargs = {'th': 0.95}
+    _test_function(
+        kornia.feature.match_smnn,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_match_fginn(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(3, 128),
+        torch.rand(3, 128),
+        torch.rand(1, 3, 2, 3),
+        torch.rand(1, 3, 2, 3),
+    )
+    trace_kwargs = {'th': 0.8, 'spatial_th': 10.0, 'mutual': False}
+    test_args = (
+        torch.rand(5, 128),
+        torch.rand(5, 128),
+        torch.rand(1, 5, 2, 3),
+        torch.rand(1, 5, 2, 3),
+    )
+    test_kwargs = {'th': 0.8, 'spatial_th': 10.0, 'mutual': False}
+    _test_function(
+        kornia.feature.match_fginn,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_match_adalam(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(3, 128),
+        torch.rand(3, 128),
+        torch.rand(1, 3, 2, 3),
+        torch.rand(1, 3, 2, 3),
+    )
+    trace_kwargs = {'config': None, 'hw1': None, 'hw2': None}
+    test_args = (
+        torch.rand(5, 128),
+        torch.rand(5, 128),
+        torch.rand(1, 5, 2, 3),
+        torch.rand(1, 5, 2, 3),
+    )
+    test_kwargs = {'config': None, 'hw1': None, 'hw2': None}
+    _test_function(
+        kornia.feature.match_adalam,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_extract_patches_from_pyramid(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 3, 32, 32),
+        torch.rand(1, 5, 2, 3),
+    )
+    trace_kwargs = {'PS': 32}
+    test_args = (
+        torch.rand(1, 3, 64, 64),  # TODO: changing the batch size of these causes the trace_graph test to fail
+        torch.rand(1, 5, 2, 3),
+    )
+    test_kwargs = {'PS': 16}
+    _test_function(
+        kornia.feature.extract_patches_from_pyramid,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_extract_patches_simple(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 3, 32, 32),
+        torch.rand(1, 5, 2, 3),
+    )
+    trace_kwargs = {'PS': 32, 'normalize_lafs_before_extraction': True}
+    test_args = (
+        torch.rand(2, 3, 64, 64),
+        torch.rand(2, 5, 2, 3),
+    )
+    test_kwargs = {'PS': 16, 'normalize_lafs_before_extraction': False}
+    _test_function(
+        kornia.feature.extract_patches_simple,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_normalize_laf(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 5, 2, 3),
+        torch.rand(1, 3, 32, 32),
+    )
+    trace_kwargs = {}
+    test_args = (
+        torch.rand(2, 5, 2, 3),
+        torch.rand(2, 3, 64, 64),
+    )
+    test_kwargs = {}
+    _test_function(
+        kornia.feature.normalize_laf,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_denormalize_laf(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 5, 2, 3),
+        torch.rand(1, 3, 32, 32),
+    )
+    trace_kwargs = {}
+    test_args = (
+        torch.rand(2, 5, 2, 3),
+        torch.rand(2, 3, 64, 64),
+    )
+    test_kwargs = {}
+    _test_function(
+        kornia.feature.denormalize_laf,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_laf_to_boundary_points(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 5, 2, 3),
+    )
+    trace_kwargs = {'n_pts': 50}
+    test_args = (
+        torch.rand(2, 5, 2, 3),
+    )
+    test_kwargs = {'n_pts': 100}
+    _test_function(
+        kornia.feature.laf_to_boundary_points,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_ellipse_to_laf(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 10, 5),
+    )
+    trace_kwargs = {}
+    test_args = (
+        torch.rand(2, 10, 5),
+    )
+    test_kwargs = {}
+    _test_function(
+        kornia.feature.ellipse_to_laf,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
+
+
+def test_make_upright(target_framework, mode, backend_compile):
+    trace_args = (
+        torch.rand(1, 5, 2, 3),
+    )
+    trace_kwargs = {'eps': 1e-9}
+    test_args = (
+        torch.rand(2, 5, 2, 3),
+    )
+    test_kwargs = {'eps': 1e-6}
+    _test_function(
+        kornia.feature.make_upright,
+        trace_args,
+        trace_kwargs,
+        test_args,
+        test_kwargs,
+        target_framework,
+        backend_compile,
+        tolerance=1e-3,
+        mode=mode,
+    )
---
+++ kornia/test_feature3.py
@@ -0,0 +1,288 @@
+from helpers import (
+    _nest_torch_tensor_to_new_framework,
+    _test_function,
+    _to_numpy_and_allclose,
+    _to_numpy_and_shape_allclose,
+)
+
+import ivy
+import kornia
+import pytest
+import torch
+
+
+# Tests #
+# ----- #
+
+def test_TFeat(target_framework, mode, backend_compile):
+    print("kornia.feature.TFeat")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledTFeat = ivy.transpile(kornia.feature.TFeat, source="torch", target=target_framework)
+
+    x = torch.rand(16, 1, 32, 32)
+    torch_out = kornia.feature.TFeat()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledTFeat()(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_SOSNet(target_framework, mode, backend_compile):
+    print("kornia.feature.SOSNet")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledSOSNet = ivy.transpile(kornia.feature.SOSNet, source="torch", target=target_framework)
+
+    x = torch.rand(8, 1, 32, 32)
+    torch_out = kornia.feature.SOSNet()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledSOSNet()(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_BlobHessian(target_framework, mode, backend_compile):
+    print("kornia.feature.BlobHessian")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledBlobHessian = ivy.transpile(kornia.feature.BlobHessian, source="torch", target=target_framework)
+
+    x = torch.rand(1, 1, 32, 32)
+    torch_out = kornia.feature.BlobHessian()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledBlobHessian()(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_CornerGFTT(target_framework, mode, backend_compile):
+    print("kornia.feature.CornerGFTT")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledCornerGFTT = ivy.transpile(kornia.feature.CornerGFTT, source="torch", target=target_framework)
+
+    x = torch.rand(1, 1, 32, 32)
+    torch_out = kornia.feature.CornerGFTT()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledCornerGFTT()(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_CornerHarris(target_framework, mode, backend_compile):
+    print("kornia.feature.CornerHarris")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledCornerHarris = ivy.transpile(kornia.feature.CornerHarris, source="torch", target=target_framework)
+
+    x = torch.rand(1, 1, 32, 32)
+    torch_out = kornia.feature.CornerHarris(x)(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledCornerHarris(transpiled_x)(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_BlobDoG(target_framework, mode, backend_compile):
+    print("kornia.feature.BlobDoG")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledBlobDoG = ivy.transpile(kornia.feature.BlobDoG, source="torch", target=target_framework)
+
+    x = torch.rand(2, 3, 3, 32, 32)
+    torch_out = kornia.feature.BlobDoG()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledBlobDoG()(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_BlobDoGSingle(target_framework, mode, backend_compile):
+    print("kornia.feature.BlobDoGSingle")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledBlobDoGSingle = ivy.transpile(kornia.feature.BlobDoGSingle, source="torch", target=target_framework)
+
+    x = torch.rand(1, 1, 32, 32)
+    torch_out = kornia.feature.BlobDoGSingle()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledBlobDoGSingle()(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_KeyNet(target_framework, mode, backend_compile):
+    print("kornia.feature.KeyNet")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledKeyNet = ivy.transpile(kornia.feature.KeyNet, source="torch", target=target_framework)
+
+    x = torch.rand(1, 1, 32, 32) * 10.
+    torch_out = kornia.feature.KeyNet()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledKeyNet()(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_MultiResolutionDetector(target_framework, mode, backend_compile):
+    print("kornia.feature.MultiResolutionDetector")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledMultiResolutionDetector = ivy.transpile(kornia.feature.MultiResolutionDetector, source="torch", target=target_framework)
+    TranspiledKeyNet = ivy.transpile(kornia.feature.KeyNet, source="torch", target=target_framework)
+
+    model = kornia.feature.KeyNet()
+    transpiled_model = TranspiledKeyNet()
+
+    x = torch.rand(1, 1, 32, 32) * 10.
+    torch_out = kornia.feature.MultiResolutionDetector(model)(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledMultiResolutionDetector(transpiled_model)(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_ScaleSpaceDetector(target_framework, mode, backend_compile):
+    print("kornia.feature.ScaleSpaceDetector")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledScaleSpaceDetector = ivy.transpile(kornia.feature.ScaleSpaceDetector, source="torch", target=target_framework)
+
+    x = torch.rand(1, 1, 32, 32) * 10.
+    torch_out = kornia.feature.ScaleSpaceDetector()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledScaleSpaceDetector()(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_KeyNetDetector(target_framework, mode, backend_compile):
+    print("kornia.feature.KeyNetDetector")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledKeyNetDetector = ivy.transpile(kornia.feature.KeyNetDetector, source="torch", target=target_framework)
+
+    x = torch.rand(1, 1, 32, 32)
+    torch_out = kornia.feature.KeyNetDetector()(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledKeyNetDetector()(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_LAFDescriptor(target_framework, mode, backend_compile):
+    print("kornia.feature.LAFDescriptor")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledLAFDescriptor = ivy.transpile(kornia.feature.LAFDescriptor, source="torch", target=target_framework)
+
+    x = torch.rand(1, 1, 64, 64)
+    lafs = torch.rand(1, 2, 2, 3)
+    torch_out = kornia.feature.LAFDescriptor()(x, lafs)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_lafs = _nest_torch_tensor_to_new_framework(lafs, target_framework)
+    transpiled_out = TranspiledLAFDescriptor()(transpiled_x, transpiled_lafs)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
+
+
+def test_SOLD2(target_framework, mode, backend_compile):
+    print("kornia.feature.SOLD2")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledSOLD2 = ivy.transpile(kornia.feature.SOLD2, source="torch", target=target_framework)
+
+    x = torch.rand(1, 1, 512, 512)
+    torch_out = kornia.feature.SOLD2(pretrained=False)(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledSOLD2(pretrained=False)(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_LocalFeature(target_framework, mode, backend_compile):
+    print("kornia.feature.LocalFeature")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledKeyNetDetector = ivy.transpile(
+        kornia.feature.KeyNetDetector, source="torch", target=target_framework
+    )
+    TranspiledLAFDescriptor = ivy.transpile(
+        kornia.feature.LAFDescriptor, source="torch", target=target_framework
+    )
+    TranspiledLocalFeature = ivy.transpile(
+        kornia.feature.LocalFeature, source="torch", target=target_framework
+    )
+
+    torch_detector = kornia.feature.KeyNetDetector()
+    torch_descriptor = kornia.feature.LAFDescriptor()
+    transpiled_detector = TranspiledKeyNetDetector()
+    transpiled_descriptor = TranspiledLAFDescriptor()
+
+    x = torch.rand(1, 1, 128, 128)
+    torch_out = kornia.feature.LocalFeature(torch_detector, torch_descriptor)(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledLocalFeature(transpiled_detector, transpiled_descriptor)(transpiled_x)
+
+    _to_numpy_and_shape_allclose(torch_out, transpiled_out)
+
+
+def test_SOLD2_detector(target_framework, mode, backend_compile):
+    print("kornia.feature.SOLD2_detector")
+
+    if backend_compile:
+        pytest.skip()
+
+    TranspiledSOLD2Detector = ivy.transpile(kornia.feature.SOLD2_detector, source="torch", target=target_framework)
+
+    x = torch.rand(1, 1, 512, 512)
+    torch_out = kornia.feature.SOLD2_detector(pretrained=False)(x)
+
+    transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
+    transpiled_out = TranspiledSOLD2Detector(pretrained=False)(transpiled_x)
+
+    _to_numpy_and_allclose(torch_out, transpiled_out)
