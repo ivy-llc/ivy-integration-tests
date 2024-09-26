3c1dd73b3cf1041600e8d850f1bccf2d343525ec
Yusha Arif <yushaa799@gmail.com>
2024-09-26 11:05:53 +0000

    fix tests: fixing the HF Swin2SR test to make it work with the JAX backend.

--- transformers/test_vision.py
+++ transformers/test_vision.py
@@ -253,9 +253,12 @@ def test_Swin2SR(target_framework, mode, backend_compile):
     # NOTE: this is in the form `integration.subsection.model`, the class submodule would just be transformers.Swin2SRModel
     print("transformers.vision.Swin2SRModel")
 
+    ivy.set_backend(target_framework)
     dataset = load_dataset("huggingface/cats-image", trust_remote_code=True)
     image = dataset["test"]["image"][0]
-    image_processor = AutoImageProcessor.from_pretrained("caidas/swin2SR-classical-sr-x2-64")
+    image_processor = AutoImageProcessor.from_pretrained(
+        "caidas/swin2SR-classical-sr-x2-64"
+    )
 
     # modify the config to avoid OOM issues
     swin2sr_config = Swin2SRConfig()
@@ -263,31 +266,46 @@ def test_Swin2SR(target_framework, mode, backend_compile):
     swin2sr_config.depths = [2, 2]
     swin2sr_config.num_heads = [2, 2]
 
-    torch_model = Swin2SRModel.from_pretrained("caidas/swin2SR-classical-sr-x2-64", config=swin2sr_config, ignore_mismatched_sizes=True)
+    torch_model = Swin2SRModel.from_pretrained(
+        "caidas/swin2SR-classical-sr-x2-64",
+        config=swin2sr_config,
+        ignore_mismatched_sizes=True,
+    )
     torch_inputs = image_processor(image, return_tensors="pt")
     with torch.no_grad():
         torch_outputs = torch_model(**torch_inputs)
     torch_last_hidden_states = torch_outputs.last_hidden_state
 
-    TFSwin2SRModel = ivy.transpile(Swin2SRModel, source="torch", target=target_framework)
-    #TODO: fix the issue with from_pretrained not working due to name-mismatch b/w PT model and translated TF model
-    translated_model = TFSwin2SRModel.from_pretrained(
-        "caidas/swin2SR-classical-sr-x2-64",
-        from_pt=True, 
-        config=swin2sr_config,
-        ignore_mismatched_sizes=True,
+    TranslatedSwin2SRModel = ivy.transpile(
+        Swin2SRModel, source="torch", target=target_framework
     )
+    if target_framework == "tensorflow":
+        # TODO: fix the issue with from_pretrained not working due to name-mismatch b/w PT model and translated TF model
+        translated_model = TranslatedSwin2SRModel.from_pretrained(
+            "caidas/swin2SR-classical-sr-x2-64",
+            from_pt=True,
+            config=swin2sr_config,
+            ignore_mismatched_sizes=True,
+        )
+    else:
+        # TODO: fix the from_pretrained issue with FlaxPretrainedModel class.
+        translated_model = TranslatedSwin2SRModel(swin2sr_config)
 
-    # TODO: remove this once from_pretrained is working properly
-    os.environ["USE_NATIVE_KERAS_LAYERS"] = "true"
+    os.environ["USE_NATIVE_FW_LAYERS"] = "true"
     os.environ["APPLY_TRANSPOSE_OPTIMIZATION"] = "true"
-    _sync_models_HF_torch_to_tf(torch_model, translated_model)
+    ivy.sync_models(torch_model, translated_model)
 
     if backend_compile:
         translated_model = _backend_compile(translated_model, target_framework)
 
-    transpiled_inputs = image_processor(image, return_tensors=_target_to_simplified(target_framework))
-    transpiled_outputs = translated_model(training=False, **transpiled_inputs)
+    transpiled_inputs = image_processor(
+        image, return_tensors=_target_to_simplified(target_framework)
+    )
+    transpiled_outputs = translated_model(**transpiled_inputs)
     transpiled_last_hidden_states = transpiled_outputs.last_hidden_state
 
-    assert np.allclose(torch_last_hidden_states.numpy(), transpiled_last_hidden_states.numpy(), atol=1e-3)
+    assert np.allclose(
+        torch_last_hidden_states.numpy(),
+        ivy.to_numpy(transpiled_last_hidden_states),
+        atol=1e-3,
+    )
