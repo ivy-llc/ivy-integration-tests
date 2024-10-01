65a4d290af46beef42853d92e24f0f14ed114725
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-10-01 17:55:20 +0100

    update dashboard stats

--- update_s2s_dashboard.py
+++ update_s2s_dashboard.py
@@ -122,10 +122,9 @@ if __name__ == "__main__":
     readme_content += f"- Total Tests Passing: {passed}\n"
     readme_content += f"- Total Tests Failing: {failed}\n"
     readme_content += f"- Percent Tests Passing: {percent_passing}%\n"
-    readme_content += f"- Successfully Transpiling to all targets: {percent_fns_passing_all_targets}%\n"
     readme_content += f"- Successfully Transpiling to TensorFlow: {percent_fns_passing_tensorflow}%\n"
     readme_content += f"- Successfully Transpiling to JAX: {percent_fns_passing_jax}%\n"
-    readme_content += f"- Successfully Transpiling to Numpy: {percent_fns_passing_numpy}%\n"
+    readme_content += f"- Successfully Transpiling to Numpy (Kornia functions only): {percent_fns_passing_numpy}%\n"
     
     for integration, submodule_functions in sorted_test_results.items():
         readme_content += f"<div style='margin-top: 35px; margin-bottom: 20px; margin-left: 25px;'>\n"
