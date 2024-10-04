29abb5eca061e6c19a4cf1804c40f09aafba8f14
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-10-04 03:57:59 +0100

    fix: print failing tests in qa workflow

--- calculate_passing_percentage.py
+++ calculate_passing_percentage.py
@@ -5,6 +5,7 @@ def main():
     passed = 0
     failed = 0
     skipped = 0
+    failing_tests = []
 
     for subdir, _, files in os.walk("artifacts"):
         for file_name in files:
@@ -20,6 +21,7 @@ def main():
                         if outcome == 'passed':
                             passed += 1
                         elif outcome == 'failed':
+                            failing_tests.append(split_line[3])
                             failed += 1
                         elif outcome == 'skipped':
                             skipped += 1
@@ -30,7 +32,10 @@ def main():
     else:
         passing_percentage = 100 * passed / total_excluding_skipped
 
-    print(f"Total tests (excluding skips): {total_excluding_skipped}")
+    print("Failing tests:")
+    for test_name in failing_tests:
+        print("-", test_name)
+    print(f"\nTotal tests (excluding skips): {total_excluding_skipped}")
     print(f"Passed: {passed}")
     print(f"Failed: {failed}")
     print(f"Passing percentage: {passing_percentage:.2f}%")
