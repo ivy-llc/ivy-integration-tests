5f39bf2f6cd1ab3df6c5807a9d9e8619c36b678c
Yusha Arif <yushaa799@gmail.com>
2024-10-18 13:01:42 +0000

    tests: change `kornia.geometry.liegroup` tests to use lazily transpiled kornia

--- kornia/geometry/test_liegroup.py
+++ kornia/geometry/test_liegroup.py
@@ -18,8 +18,7 @@ def test_So3(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledSo3 = ivy.transpile(kornia.geometry.liegroup.So3, source="torch", target=target_framework)
-    TranspiledQuaternion = ivy.transpile(kornia.geometry.quaternion.Quaternion, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
 
     # Initialize a Quaternion and create an So3 object
     quaternion_data = torch.tensor([1., 0., 0., 0.])
@@ -27,8 +26,8 @@ def test_So3(target_framework, mode, backend_compile):
     torch_so3 = kornia.geometry.liegroup.So3(torch_quaternion)
 
     # Transpile the So3 class
-    transpiled_quaternion = TranspiledQuaternion(_nest_torch_tensor_to_new_framework(quaternion_data, target_framework))
-    transpiled_so3 = TranspiledSo3(transpiled_quaternion)
+    transpiled_quaternion = transpiled_kornia.geometry.quaternion.Quaternion(_nest_torch_tensor_to_new_framework(quaternion_data, target_framework))
+    transpiled_so3 = transpiled_kornia.geometry.liegroup.So3(transpiled_quaternion)
 
     # Test .matrix()
     torch_matrix = torch_so3.matrix()
@@ -51,8 +50,8 @@ def test_So3(target_framework, mode, backend_compile):
     other_torch_so3 = kornia.geometry.liegroup.So3(other_torch_quaternion)
 
     transpiled_other_quaternion = _nest_torch_tensor_to_new_framework(other_quaternion_data, target_framework)
-    transpiled_other_quaternion_obj = ivy.transpile(kornia.geometry.quaternion.Quaternion, source="torch", target=target_framework)(transpiled_other_quaternion)
-    transpiled_other_so3 = TranspiledSo3(transpiled_other_quaternion_obj)
+    transpiled_other_quaternion_obj = transpiled_kornia.geometry.quaternion.Quaternion(transpiled_other_quaternion)
+    transpiled_other_so3 = transpiled_kornia.geometry.liegroup.So3(transpiled_other_quaternion_obj)
 
     torch_composed_so3 = torch_so3 * other_torch_so3
     transpiled_composed_so3 = transpiled_so3 * transpiled_other_so3
@@ -68,7 +67,7 @@ def test_So3(target_framework, mode, backend_compile):
     transpiled_rotation_matrix = _nest_torch_tensor_to_new_framework(rotation_matrix, target_framework)
 
     torch_from_matrix = kornia.geometry.liegroup.So3.from_matrix(rotation_matrix)
-    transpiled_from_matrix = TranspiledSo3.from_matrix(transpiled_rotation_matrix)
+    transpiled_from_matrix = transpiled_kornia.geometry.liegroup.So3.from_matrix(transpiled_rotation_matrix)
     _to_numpy_and_allclose(torch_from_matrix.q.data, transpiled_from_matrix.q.data)
 
     # Test .exp()
@@ -76,7 +75,7 @@ def test_So3(target_framework, mode, backend_compile):
     transpiled_exp_vector = _nest_torch_tensor_to_new_framework(exp_vector, target_framework)
 
     torch_exp = kornia.geometry.liegroup.So3.exp(exp_vector)
-    transpiled_exp = TranspiledSo3.exp(transpiled_exp_vector)
+    transpiled_exp = transpiled_kornia.geometry.liegroup.So3.exp(transpiled_exp_vector)
     _to_numpy_and_allclose(torch_exp.q.data, transpiled_exp.q.data)
 
 
@@ -91,12 +90,11 @@ def test_Se3(target_framework, mode, backend_compile):
     torch_quaternion = kornia.geometry.quaternion.Quaternion(quaternion_data)
     torch_se3 = kornia.geometry.liegroup.Se3(torch_quaternion, translation_data)
 
-    TranspiledSe3 = ivy.transpile(kornia.geometry.liegroup.Se3, source="torch", target=target_framework)
-    TranspiledQuaternion = ivy.transpile(kornia.geometry.quaternion.Quaternion, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
 
     transpiled_translation = _nest_torch_tensor_to_new_framework(translation_data, target_framework)
-    transpiled_quaternion = TranspiledQuaternion(_nest_torch_tensor_to_new_framework(quaternion_data, target_framework))
-    transpiled_se3 = TranspiledSe3(transpiled_quaternion, transpiled_translation)
+    transpiled_quaternion = transpiled_kornia.geometry.quaternion.Quaternion(_nest_torch_tensor_to_new_framework(quaternion_data, target_framework))
+    transpiled_se3 = transpiled_kornia.geometry.liegroup.Se3(transpiled_quaternion, transpiled_translation)
 
     # Test .matrix()
     torch_matrix = torch_se3.matrix()
@@ -122,8 +120,8 @@ def test_Se3(target_framework, mode, backend_compile):
 
     transpiled_other_quaternion = _nest_torch_tensor_to_new_framework(other_quaternion_data, target_framework)
     transpiled_other_translation = _nest_torch_tensor_to_new_framework(other_translation_data, target_framework)
-    transpiled_other_quaternion_obj = ivy.transpile(kornia.geometry.quaternion.Quaternion, source="torch", target=target_framework)(transpiled_other_quaternion)
-    transpiled_other_se3 = TranspiledSe3(transpiled_other_quaternion_obj, transpiled_other_translation)
+    transpiled_other_quaternion_obj = transpiled_kornia.geometry.quaternion.Quaternion(transpiled_other_quaternion)
+    transpiled_other_se3 = transpiled_kornia.geometry.liegroup.Se3(transpiled_other_quaternion_obj, transpiled_other_translation)
 
     torch_composed_se3 = torch_se3 * other_torch_se3
     transpiled_composed_se3 = transpiled_se3 * transpiled_other_se3
@@ -140,7 +138,7 @@ def test_Se3(target_framework, mode, backend_compile):
     transpiled_rotation_translation_matrix = _nest_torch_tensor_to_new_framework(rotation_translation_matrix, target_framework)
 
     torch_from_matrix = kornia.geometry.liegroup.Se3.from_matrix(rotation_translation_matrix)
-    transpiled_from_matrix = TranspiledSe3.from_matrix(transpiled_rotation_translation_matrix)
+    transpiled_from_matrix = transpiled_kornia.geometry.liegroup.Se3.from_matrix(transpiled_rotation_translation_matrix)
     _to_numpy_and_allclose(torch_from_matrix.r.q.data, transpiled_from_matrix.r.q.data)
     _to_numpy_and_allclose(torch_from_matrix.t, transpiled_from_matrix.t)
 
@@ -149,7 +147,7 @@ def test_Se3(target_framework, mode, backend_compile):
     transpiled_exp_vector = _nest_torch_tensor_to_new_framework(exp_vector, target_framework)
 
     torch_exp = kornia.geometry.liegroup.Se3.exp(exp_vector)
-    transpiled_exp = TranspiledSe3.exp(transpiled_exp_vector)
+    transpiled_exp = transpiled_kornia.geometry.liegroup.Se3.exp(transpiled_exp_vector)
     _to_numpy_and_allclose(torch_exp.r.q.data, transpiled_exp.r.q.data)
     _to_numpy_and_allclose(torch_exp.t, transpiled_exp.t)
 
@@ -160,14 +158,15 @@ def test_So2(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
+
     real_part = torch.tensor([1.0], requires_grad=True)
     imaginary_part = torch.tensor([2.0], requires_grad=True)
     complex_number = torch.complex(real_part, imaginary_part)
     torch_so2 = kornia.geometry.liegroup.So2(complex_number)
 
-    TranspiledSo2 = ivy.transpile(kornia.geometry.liegroup.So2, source="torch", target=target_framework)
     transpiled_complex_number = _nest_torch_tensor_to_new_framework(complex_number, target_framework)
-    transpiled_so2 = TranspiledSo2(transpiled_complex_number)
+    transpiled_so2 = transpiled_kornia.geometry.liegroup.So2(transpiled_complex_number)
 
     # Test .matrix()
     torch_matrix = torch_so2.matrix()
@@ -191,7 +190,7 @@ def test_So2(target_framework, mode, backend_compile):
     other_torch_so2 = kornia.geometry.liegroup.So2(other_complex_number)
 
     transpiled_other_complex_number = _nest_torch_tensor_to_new_framework(other_complex_number, target_framework)
-    transpiled_other_so2 = TranspiledSo2(transpiled_other_complex_number)
+    transpiled_other_so2 = transpiled_kornia.geometry.liegroup.So2(transpiled_other_complex_number)
 
     torch_composed_so2 = torch_so2 * other_torch_so2
     transpiled_composed_so2 = transpiled_so2 * transpiled_other_so2
@@ -207,7 +206,7 @@ def test_So2(target_framework, mode, backend_compile):
     transpiled_rotation_matrix = _nest_torch_tensor_to_new_framework(rotation_matrix, target_framework)
 
     torch_from_matrix = kornia.geometry.liegroup.So2.from_matrix(rotation_matrix)
-    transpiled_from_matrix = TranspiledSo2.from_matrix(transpiled_rotation_matrix)
+    transpiled_from_matrix = transpiled_kornia.geometry.liegroup.So2.from_matrix(transpiled_rotation_matrix)
     _to_numpy_and_allclose(torch_from_matrix.z, transpiled_from_matrix.z)
 
     # Test .exp()
@@ -215,12 +214,12 @@ def test_So2(target_framework, mode, backend_compile):
     transpiled_theta = _nest_torch_tensor_to_new_framework(theta, target_framework)
 
     torch_exp = kornia.geometry.liegroup.So2.exp(theta)
-    transpiled_exp = TranspiledSo2.exp(transpiled_theta)
+    transpiled_exp = transpiled_kornia.geometry.liegroup.So2.exp(transpiled_theta)
     _to_numpy_and_allclose(torch_exp.z, transpiled_exp.z)
 
     # Test .identity()
     torch_identity = kornia.geometry.liegroup.So2.identity()
-    transpiled_identity = TranspiledSo2.identity()
+    transpiled_identity = transpiled_kornia.geometry.liegroup.So2.identity()
     _to_numpy_and_allclose(torch_identity.z, transpiled_identity.z)
 
     # Test .hat() and .vee()
@@ -228,11 +227,11 @@ def test_So2(target_framework, mode, backend_compile):
     transpiled_hat_theta = _nest_torch_tensor_to_new_framework(hat_theta, target_framework)
 
     torch_hat = kornia.geometry.liegroup.So2.hat(hat_theta)
-    transpiled_hat = TranspiledSo2.hat(transpiled_hat_theta)
+    transpiled_hat = transpiled_kornia.geometry.liegroup.So2.hat(transpiled_hat_theta)
     _to_numpy_and_allclose(torch_hat, transpiled_hat)
 
     torch_vee = kornia.geometry.liegroup.So2.vee(torch_hat)
-    transpiled_vee = TranspiledSo2.vee(transpiled_hat)
+    transpiled_vee = transpiled_kornia.geometry.liegroup.So2.vee(transpiled_hat)
     _to_numpy_and_allclose(torch_vee, transpiled_vee)
 
 
@@ -242,16 +241,15 @@ def test_Se2(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
+
     so2_rotation = kornia.geometry.liegroup.So2.identity(1)
     translation_vector = torch.ones((1, 2), requires_grad=True)
     torch_se2 = kornia.geometry.liegroup.Se2(so2_rotation, translation_vector)
 
-    TranspiledSe2 = ivy.transpile(kornia.geometry.liegroup.Se2, source="torch", target=target_framework)
-    TranspiledSo2 = ivy.transpile(kornia.geometry.liegroup.So2, source="torch", target=target_framework)
-
-    transpiled_so2_rotation = TranspiledSo2.identity(1)
+    transpiled_so2_rotation = transpiled_kornia.geometry.liegroup.Se2.identity(1)
     transpiled_translation_vector = _nest_torch_tensor_to_new_framework(translation_vector, target_framework)
-    transpiled_se2 = TranspiledSe2(transpiled_so2_rotation, transpiled_translation_vector)
+    transpiled_se2 = transpiled_kornia.geometry.liegroup.Se2(transpiled_so2_rotation, transpiled_translation_vector)
 
     # Test .matrix()
     torch_matrix = torch_se2.matrix()
@@ -274,9 +272,9 @@ def test_Se2(target_framework, mode, backend_compile):
     other_translation_vector = torch.tensor([[0.5, 0.5]], requires_grad=True)
     other_torch_se2 = kornia.geometry.liegroup.Se2(other_so2_rotation, other_translation_vector)
 
-    transpiled_other_so2_rotation = TranspiledSo2.identity(1)
+    transpiled_other_so2_rotation = transpiled_kornia.geometry.liegroup.So2.identity(1)
     transpiled_other_translation_vector = _nest_torch_tensor_to_new_framework(other_translation_vector, target_framework)
-    transpiled_other_se2 = TranspiledSe2(transpiled_other_so2_rotation, transpiled_other_translation_vector)
+    transpiled_other_se2 = transpiled_kornia.geometry.liegroup.Se2(transpiled_other_so2_rotation, transpiled_other_translation_vector)
 
     torch_composed_se2 = torch_se2 * other_torch_se2
     transpiled_composed_se2 = transpiled_se2 * transpiled_other_se2
@@ -293,7 +291,7 @@ def test_Se2(target_framework, mode, backend_compile):
     transpiled_rotation_matrix = _nest_torch_tensor_to_new_framework(rotation_matrix, target_framework)
 
     torch_from_matrix = kornia.geometry.liegroup.Se2.from_matrix(rotation_matrix)
-    transpiled_from_matrix = TranspiledSe2.from_matrix(transpiled_rotation_matrix)
+    transpiled_from_matrix = transpiled_kornia.geometry.liegroup.Se2.from_matrix(transpiled_rotation_matrix)
     _to_numpy_and_allclose(torch_from_matrix.rotation.z, transpiled_from_matrix.rotation.z)
     _to_numpy_and_allclose(torch_from_matrix.translation, transpiled_from_matrix.translation)
 
@@ -302,13 +300,13 @@ def test_Se2(target_framework, mode, backend_compile):
     transpiled_v = _nest_torch_tensor_to_new_framework(v, target_framework)
 
     torch_exp = kornia.geometry.liegroup.Se2.exp(v)
-    transpiled_exp = TranspiledSe2.exp(transpiled_v)
+    transpiled_exp = transpiled_kornia.geometry.liegroup.Se2.exp(transpiled_v)
     _to_numpy_and_allclose(torch_exp.rotation.z, transpiled_exp.rotation.z)
     _to_numpy_and_allclose(torch_exp.translation, transpiled_exp.translation)
 
     # Test .identity()
     torch_identity = kornia.geometry.liegroup.Se2.identity(1)
-    transpiled_identity = TranspiledSe2.identity(1)
+    transpiled_identity = transpiled_kornia.geometry.liegroup.Se2.identity(1)
     _to_numpy_and_allclose(torch_identity.rotation.z, transpiled_identity.rotation.z)
     _to_numpy_and_allclose(torch_identity.translation.data, transpiled_identity.translation.data)
 
@@ -317,11 +315,11 @@ def test_Se2(target_framework, mode, backend_compile):
     transpiled_hat_v = _nest_torch_tensor_to_new_framework(hat_v, target_framework)
 
     torch_hat = kornia.geometry.liegroup.Se2.hat(hat_v)
-    transpiled_hat = TranspiledSe2.hat(transpiled_hat_v)
+    transpiled_hat = transpiled_kornia.geometry.liegroup.Se2.hat(transpiled_hat_v)
     _to_numpy_and_allclose(torch_hat, transpiled_hat)
 
     torch_vee = kornia.geometry.liegroup.Se2.vee(torch_hat)
-    transpiled_vee = TranspiledSe2.vee(transpiled_hat)
+    transpiled_vee = transpiled_kornia.geometry.liegroup.Se2.vee(transpiled_hat)
     _to_numpy_and_allclose(torch_vee, transpiled_vee)
 
     # Test properties .rotation and .translation
@@ -336,13 +334,13 @@ def test_Se2(target_framework, mode, backend_compile):
     transpiled_y_translation = _nest_torch_tensor_to_new_framework(y_translation, target_framework)
 
     torch_trans_se2 = kornia.geometry.liegroup.Se2.trans(x_translation, y_translation)
-    transpiled_trans_se2 = TranspiledSe2.trans(transpiled_x_translation, transpiled_y_translation)
+    transpiled_trans_se2 = transpiled_kornia.geometry.liegroup.Se2.trans(transpiled_x_translation, transpiled_y_translation)
     _to_numpy_and_allclose(torch_trans_se2.translation, transpiled_trans_se2.translation)
 
     torch_trans_x_se2 = kornia.geometry.liegroup.Se2.trans_x(x_translation)
-    transpiled_trans_x_se2 = TranspiledSe2.trans_x(transpiled_x_translation)
+    transpiled_trans_x_se2 = transpiled_kornia.geometry.liegroup.Se2.trans_x(transpiled_x_translation)
     _to_numpy_and_allclose(torch_trans_x_se2.translation[:, 0], transpiled_trans_x_se2.translation[:, 0])
 
     torch_trans_y_se2 = kornia.geometry.liegroup.Se2.trans_y(y_translation)
-    transpiled_trans_y_se2 = TranspiledSe2.trans_y(transpiled_y_translation)
+    transpiled_trans_y_se2 = transpiled_kornia.geometry.liegroup.Se2.trans_y(transpiled_y_translation)
     _to_numpy_and_allclose(torch_trans_y_se2.translation[:, 1], transpiled_trans_y_se2.translation[:, 1])
