81c50e05cf4039f420106ed38af791b89f565e83
Yusha Arif <yushaa799@gmail.com>
2024-10-18 14:09:40 +0000

    tests: change `kornia.feature5` tests to use lazily transpiled kornia

--- kornia/test_feature5.py
+++ kornia/test_feature5.py
@@ -20,7 +20,7 @@ def test_LAFOrienter(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledLAFOrienter = ivy.transpile(kornia.feature.LAFOrienter, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
 
     laf = torch.rand(1, 2, 2, 3)
     img = torch.rand(1, 1, 32, 32)
@@ -30,7 +30,7 @@ def test_LAFOrienter(target_framework, mode, backend_compile):
     model = kornia.feature.LAFOrienter()
     torch_out = model(laf, img)
 
-    transpiled_model = TranspiledLAFOrienter()
+    transpiled_model = transpiled_kornia.feature.LAFOrienter()
     if target_framework == "tensorflow":
         # build the layers 
         transpiled_model(transpiled_laf, transpiled_img)
@@ -48,7 +48,7 @@ def test_PatchDominantGradientOrientation(target_framework, mode, backend_compil
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledPatchDominantGradientOrientation = ivy.transpile(kornia.feature.PatchDominantGradientOrientation, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
 
     patch = torch.rand(10, 1, 32, 32)
     transpiled_patch = _nest_torch_tensor_to_new_framework(patch, target_framework)
@@ -56,7 +56,7 @@ def test_PatchDominantGradientOrientation(target_framework, mode, backend_compil
     model = kornia.feature.PatchDominantGradientOrientation()
     torch_out = model(patch)
 
-    transpiled_model = TranspiledPatchDominantGradientOrientation()
+    transpiled_model = transpiled_kornia.feature.PatchDominantGradientOrientation()
     if target_framework == "tensorflow":
         # build the layers 
         transpiled_model(transpiled_patch)
@@ -74,7 +74,7 @@ def test_OriNet(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledOriNet = ivy.transpile(kornia.feature.OriNet, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
 
     patch = torch.rand(16, 1, 32, 32)
     transpiled_patch = _nest_torch_tensor_to_new_framework(patch, target_framework)
@@ -82,7 +82,7 @@ def test_OriNet(target_framework, mode, backend_compile):
     model = kornia.feature.OriNet()
     torch_out = model(patch)
 
-    transpiled_model = TranspiledOriNet()
+    transpiled_model = transpiled_kornia.feature.OriNet()
     if target_framework == "tensorflow":
         # build the layers 
         transpiled_model(transpiled_patch)
@@ -100,7 +100,7 @@ def test_LAFAffNetShapeEstimator(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledLAFAffNetShapeEstimator = ivy.transpile(kornia.feature.LAFAffNetShapeEstimator, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
 
     laf = torch.rand(10, 2, 2, 3)
     img = torch.rand(10, 1, 32, 32)
@@ -108,7 +108,7 @@ def test_LAFAffNetShapeEstimator(target_framework, mode, backend_compile):
 
     transpiled_laf = _nest_torch_tensor_to_new_framework(laf, target_framework)
     transpiled_img = _nest_torch_tensor_to_new_framework(img, target_framework)
-    transpiled_out = TranspiledLAFAffNetShapeEstimator()(transpiled_laf, transpiled_img)
+    transpiled_out = transpiled_kornia.feature.LAFAffNetShapeEstimator()(transpiled_laf, transpiled_img)
 
     _to_numpy_and_shape_allclose(torch_out, transpiled_out)
 
@@ -119,13 +119,13 @@ def test_FilterResponseNorm2d(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledFilterResponseNorm2d = ivy.transpile(kornia.feature.FilterResponseNorm2d, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
 
     x = torch.rand(1, 3, 8, 8)
     torch_out = kornia.feature.FilterResponseNorm2d(3)(x)
 
     transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
-    transpiled_out = TranspiledFilterResponseNorm2d(3)(transpiled_x)
+    transpiled_out = transpiled_kornia.feature.FilterResponseNorm2d(3)(transpiled_x)
 
     _to_numpy_and_allclose(torch_out, transpiled_out)
 
@@ -136,13 +136,13 @@ def test_TLU(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledTLU = ivy.transpile(kornia.feature.TLU, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
 
     x = torch.rand(1, 3, 8, 8)
     torch_out = kornia.feature.TLU(3)(x)
 
     transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
-    transpiled_out = TranspiledTLU(3)(transpiled_x)
+    transpiled_out = transpiled_kornia.feature.TLU(3)(transpiled_x)
 
     _to_numpy_and_allclose(torch_out, transpiled_out)
 
@@ -153,13 +153,13 @@ def test_DeFMO(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledDeFMO = ivy.transpile(kornia.feature.DeFMO, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
 
     x = torch.rand(2, 6, 240, 320)
     torch_out = kornia.feature.DeFMO()(x)
 
     transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
-    transpiled_out = TranspiledDeFMO()(transpiled_x)
+    transpiled_out = transpiled_kornia.feature.DeFMO()(transpiled_x)
 
     _to_numpy_and_shape_allclose(torch_out, transpiled_out)
 
@@ -170,7 +170,7 @@ def test_DeDoDe(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledDeDoDe = ivy.transpile(kornia.feature.DeDoDe, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
 
     x = torch.rand(1, 3, 256, 256)
     transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
@@ -179,7 +179,7 @@ def test_DeDoDe(target_framework, mode, backend_compile):
     torch_out = model(x)
 
     ivy.set_backend(target_framework)
-    transpiled_model = TranspiledDeDoDe(amp_dtype=ivy.as_native_dtype("float32"))
+    transpiled_model = transpiled_kornia.feature.DeDoDe(amp_dtype=ivy.as_native_dtype("float32"))
     if target_framework == "tensorflow":
         # build the layers 
         transpiled_model(transpiled_x)
@@ -197,7 +197,7 @@ def test_DISK(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledDISK = ivy.transpile(kornia.feature.DISK, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
 
     x = torch.rand(1, 3, 256, 256)
     transpiled_x = _nest_torch_tensor_to_new_framework(x, target_framework)
@@ -205,7 +205,7 @@ def test_DISK(target_framework, mode, backend_compile):
     model = kornia.feature.DISK()
     torch_out = model(x)
     
-    transpiled_model = TranspiledDISK()
+    transpiled_model = transpiled_kornia.feature.DISK()
     if target_framework == "tensorflow":
         # build the layers 
         transpiled_model(transpiled_x)
@@ -225,6 +225,7 @@ def test_DISKFeatures(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
+    transpiled_kornia = ivy.transpile(kornia, source="torch", target=target_framework)
     keypoints = torch.rand(100, 2)
     descriptors = torch.rand(100, 128)
     detection_scores = torch.rand(100)
@@ -233,7 +234,7 @@ def test_DISKFeatures(target_framework, mode, backend_compile):
     transpiled_keypoints = _nest_torch_tensor_to_new_framework(keypoints, target_framework)
     transpiled_descriptors = _nest_torch_tensor_to_new_framework(descriptors, target_framework)
     transpiled_detection_scores = _nest_torch_tensor_to_new_framework(detection_scores, target_framework)
-    transpiled_out = kornia.feature.DISKFeatures(transpiled_keypoints, transpiled_descriptors, transpiled_detection_scores)
+    transpiled_out = transpiled_kornia.feature.DISKFeatures(transpiled_keypoints, transpiled_descriptors, transpiled_detection_scores)
 
     _to_numpy_and_shape_allclose(torch_out.keypoints, transpiled_out.keypoints)
     _to_numpy_and_shape_allclose(torch_out.descriptors, transpiled_out.descriptors)
