2d8e696193531c28f61371ca546cb30e811d3f67
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-10-19 08:24:09 +0100

    tests: change kornia.sensors tests to use lazily transpiled kornia

--- kornia/test_sensors.py
+++ kornia/test_sensors.py
@@ -7,12 +7,8 @@ from helpers import (
 
 import ivy
 import kornia
-from kornia.sensors import camera
-from kornia.sensors.camera import (
-    CameraModel,
-    CameraModelType,
-    PinholeModel,
-)
+import kornia.sensors
+import kornia.sensors.camera
 import pytest
 import torch
 
@@ -26,19 +22,17 @@ def test_CameraModel(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledImageSize = ivy.transpile(kornia.image.ImageSize, source="torch", target=target_framework)
-    TranspiledCameraModelType = ivy.transpile(CameraModelType, source="torch", target=target_framework)
-    TranspiledCameraModel = ivy.transpile(CameraModel, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, target=target_framework)
 
-    torch_cam = CameraModel(
+    torch_cam = kornia.sensors.camera.CameraModel(
         kornia.image.ImageSize(480, 640),
-        CameraModelType.ORTHOGRAPHIC,
+        kornia.sensors.camera.CameraModelType.ORTHOGRAPHIC,
         torch.Tensor([328., 328., 320., 240.]),
     )
-    transpiled_cam = TranspiledCameraModel(
-        TranspiledImageSize(480, 640),
-        TranspiledCameraModelType.ORTHOGRAPHIC,
-        torch.Tensor([328., 328., 320., 240.]),
+    transpiled_cam = transpiled_kornia.sensors.camera.CameraModel(
+        transpiled_kornia.image.ImageSize(480, 640),
+        transpiled_kornia.sensors.camera.CameraModelTyp.ORTHOGRAPHIC,
+        _array_to_new_backend(torch.Tensor([328., 328., 320., 240.]), target_framework),
     )
 
     orig_np = _nest_array_to_numpy(torch_cam.params)
@@ -56,16 +50,15 @@ def test_PinholeModel(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledImageSize = ivy.transpile(kornia.image.ImageSize, source="torch", target=target_framework)
-    TranspiledPinholeModel = ivy.transpile(PinholeModel, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, target=target_framework)
 
-    torch_cam = PinholeModel(
+    torch_cam = kornia.sensors.camera.PinholeModel(
         kornia.image.ImageSize(480, 640),
         torch.Tensor([328., 328., 320., 240.]),
     )
-    transpiled_cam = TranspiledPinholeModel(
-        TranspiledImageSize(480, 640),
-        torch.Tensor([328., 328., 320., 240.]),
+    transpiled_cam = transpiled_kornia.sensors.camera.PinholeModel(
+        transpiled_kornia.image.ImageSize(480, 640),
+        _array_to_new_backend(torch.Tensor([328., 328., 320., 240.]), target_framework),
     )
 
     orig_np = _nest_array_to_numpy(torch_cam.params)
@@ -81,26 +74,25 @@ def test_AffineTransform(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledVector2 = ivy.transpile(kornia.geometry.vector.Vector2, source="torch", target=target_framework)
-    TranspiledAffineTransform = ivy.transpile(camera.distortion_model.AffineTransform, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, target=target_framework)
 
     params = torch.Tensor([1., 2., 3., 4.])
     points = kornia.geometry.vector.Vector2.from_coords(1., 2.)
-    torch_out = camera.distortion_model.AffineTransform().distort(params, points).data
+    torch_out = kornia.sensors.camera.distortion_model.AffineTransform().distort(params, points).data
 
     transpiled_params = _array_to_new_backend(params, target_framework)
-    transpiled_points = TranspiledVector2.from_coords(1., 2.)
-    transpiled_out = TranspiledAffineTransform().distort(transpiled_params, transpiled_points).data
+    transpiled_points = transpiled_kornia.geometry.vector.Vector2.from_coords(1., 2.)
+    transpiled_out = transpiled_kornia.sensors.camera.distortion_model.AffineTransform().distort(transpiled_params, transpiled_points).data
 
     _to_numpy_and_allclose(torch_out, transpiled_out)
 
     params = torch.Tensor([1., 2., 3., 4.])
     points = kornia.geometry.vector.Vector2.from_coords(1., 2.)
-    torch_out = camera.distortion_model.AffineTransform().undistort(params, points).data
+    torch_out = kornia.sensors.camera.distortion_model.AffineTransform().undistort(params, points).data
 
     transpiled_params = _array_to_new_backend(params, target_framework)
-    transpiled_points = TranspiledVector2.from_coords(1., 2.)
-    transpiled_out = TranspiledAffineTransform().undistort(transpiled_params, transpiled_points).data
+    transpiled_points = transpiled_kornia.geometry.vector.Vector2.from_coords(1., 2.)
+    transpiled_out = transpiled_kornia.sensors.camera.distortion_model.AffineTransform().undistort(transpiled_params, transpiled_points).data
 
     _to_numpy_and_allclose(torch_out, transpiled_out)
 
@@ -111,22 +103,20 @@ def test_Z1Projection(target_framework, mode, backend_compile):
     if backend_compile or target_framework == "numpy":
         pytest.skip()
 
-    TranspiledVector2 = ivy.transpile(kornia.geometry.vector.Vector2, source="torch", target=target_framework)
-    TranspiledVector3 = ivy.transpile(kornia.geometry.vector.Vector3, source="torch", target=target_framework)
-    TranspiledZ1Projection = ivy.transpile(camera.projection_model.Z1Projection, source="torch", target=target_framework)
+    transpiled_kornia = ivy.transpile(kornia, target=target_framework)
 
     points = kornia.geometry.vector.Vector3.from_coords(1., 2., 3.)
-    torch_out = camera.projection_model.Z1Projection().project(points).data
+    torch_out = kornia.sensors.camera.projection_model.Z1Projection().project(points).data
 
-    transpiled_points = TranspiledVector3.from_coords(1., 2., 3.)
-    transpiled_out = TranspiledZ1Projection().project(transpiled_points).data
+    transpiled_points = transpiled_kornia.geometry.vector.Vector3.from_coords(1., 2., 3.)
+    transpiled_out = transpiled_kornia.sensors.camera.projection_model.Z1Projection().project(transpiled_points).data
 
     _to_numpy_and_allclose(torch_out, transpiled_out)
 
     points = kornia.geometry.vector.Vector2.from_coords(1., 2.)
-    torch_out = camera.projection_model.Z1Projection().unproject(points, 3).data
+    torch_out = kornia.sensors.camera.projection_model.Z1Projection().unproject(points, 3).data
 
-    transpiled_points = TranspiledVector2.from_coords(1., 2.)
-    transpiled_out = TranspiledZ1Projection().unproject(transpiled_points, 3).data
+    transpiled_points = transpiled_kornia.geometry.vector.Vector2.from_coords(1., 2.)
+    transpiled_out = transpiled_kornia.sensors.camera.projection_model.Z1Projection().unproject(transpiled_points, 3).data
 
     _to_numpy_and_allclose(torch_out, transpiled_out)
